2b89bda84f38e1be1ee6503eb7b0cf8775d509c7
Piyawat Choeychit <59070103@it.kmitl.ac.th>
2016-11-01 15:02:22 +0700

    เปลี่ยนค่า Return +1

--- beer.py
+++ beer.py
@@ -2,12 +2,6 @@ def findArea():
     pi = 3.14
     r = 402
     area = pi * r * r
-    return area
-<<<<<<< HEAD
-#Test Comment
-=======
-#Test Comment 6789
-#test di sus
->>>>>>> dev2
+    return area+1
 
 print(findArea())
